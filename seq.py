2ff4c047f41eaf4070061db08884a64a6f2089bf
Ostoja <momomje@yahoo.com>
2018-11-23 12:00:49 +0100

    Dodati argumenti dt i tmax.

--- seq.py
+++ seq.py
@@ -12,6 +12,8 @@ from scipy.integrate import odeint
 # The gravitational acceleration (m.s-2).
 g = 9.81
 DEFAULT_RESOLUTION = 5
+DEFAULT_DT = 0.01
+DEFAULT_TMAX = 30
 
 def deriv(y, t, L1, L2, m1, m2):
     """Return the first derivatives of y = theta1, z1, theta2, z2."""
@@ -42,13 +44,13 @@ def solve(L1, L2, m1, m2, tmax, dt, y0):
 
     return theta1, theta2, x1, y1, x2, y2
 
-def simulate_pendulum(theta_resolution, datafile):
+def simulate_pendulum(theta_resolution, datafile, tmax, dt):
     # Pendulum rod lengths (m), bob masses (kg).
     L1, L2 = 1.0, 1.0
     m1, m2 = 1.0, 1.0
 
     # Maximum time, time point spacings (all in s).
-    tmax, dt = 30.0, 0.01
+    #tmax, dt = 30.0, 0.01
 
     for theta1_init in np.linspace(0, 2*np.pi, theta_resolution):
         for theta2_init in np.linspace(0, 2*np.pi, theta_resolution):
@@ -87,7 +89,22 @@ def main():
         default=DEFAULT_RESOLUTION,
         help="Resolution, %d by default" % DEFAULT_RESOLUTION
     )
-    
+    parser.add_argument(
+        '-tm',
+        '--tmax',
+        metavar='NUM',
+        type=float,
+        default=DEFAULT_TMAX,
+        help="Tmax, %d by default" % DEFAULT_TMAX
+    )
+    parser.add_argument(
+        '-dt',
+        '--dt',
+        metavar='NUM',
+        type=float,
+        default=DEFAULT_DT,
+        help="Step, %d by default" % DEFAULT_DT
+    )
     args = parser.parse_args()
     
     print 'Printam '
@@ -106,7 +123,7 @@ def main():
     #    purge_integral_db_cache=args.purge_integral_db_cache,
     #    paginate_by=args.paginate_by,
     #)
-    simulate_pendulum(args.resolution, args.data_file)
+    simulate_pendulum(args.resolution, args.data_file, args.tmax, args.dt)
 
 if __name__ == "__main__":
     main()
